2899f1e5d109fcd97f41d6de68614a72c12f215d
Ermain <paulermain@gmail.com>
2018-06-15 13:06:23 -0400

    Begin implementing the upload functionality for the user, the view still needs to be constructed

---
+++ SimpleImageGallery/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Models;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            // The user will be presented with a form which will later be posted
+            var model = new UploadImageModel();
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SimpleImageGallery/Models/UploadImageModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleImageGallery.Models
+{
+    public class UploadImageModel
+    {
+        public string Title { get; set; }
+
+        // The user will specify comma-seperated tags
+        public string Tags { get; set; }
+
+        // Represents the form which the user will fill
+        public IFormFile ImageUpload { get; set; }
+    }
+}
